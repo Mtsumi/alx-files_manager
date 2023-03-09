21a355b5395cc332ec5be9cedb8e096bc29a7c33
Mtsumi <mtsumijr@gmail.com>
2023-03-09 17:46:42 +0300

    README edit

---
+++ utils/db.js
@@ -0,0 +1,10 @@
+import { MongoClient } from 'mongodb';
+
+const defaultHost = localhost;
+const defaultPort = 27017;
+const defaultDB = files_manager;
+
+class DBClient{
+    constructor() {
+    }
+}
